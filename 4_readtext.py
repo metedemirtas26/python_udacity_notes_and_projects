f290a35aecf3b6c743690aa94f795a8127576f78
Mt <metedemirtas26@gmail.com>
2019-07-07 11:59:50 +0200

    modified:   4_readtext.py 	modified:   movie_quotes.txt

--- 4_readtext.py
+++ 4_readtext.py
@@ -1,7 +1,22 @@
+import urllib
+
 def read_text():
     quotes = open("/home/mt/python_udacity_notes_and_projects/movie_quotes.txt")
     contents_of_file = quotes.read()
-    print(contents_of_file)
+    #print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=s=" + text_to_check)
+    output = connection.read()
+    #print(output)
+    connection.close()
+    if "true" in output:
+        print("Alert!")
+    elif "false" in output:
+        print("This document has no curse words!")
+    else:
+        print("Could not scan the document properly")
 read_text()
 
