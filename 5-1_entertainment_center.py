3c1ca5de5e95a9ca27ac4d39add647ac909ba85d
Mt <metedemirtas26@gmail.com>
2019-07-07 16:22:43 +0200

    modified:   5-1_entertainment_center.py

--- 5-1_entertainment_center.py
+++ 5-1_entertainment_center.py
@@ -2,8 +2,9 @@
 # sudo apt-get update
 # sudo apt-get install python-wxgtk-media3.0
 
-import media 
-# That library is not working now, I will write, when I find the solving
+#from pygraphics import media
+import media
+# That library is not working in my computer now, I will write, when I find the solving
 
 toy_story = media.Movie("Toy Story",
                         "A story of..", 
